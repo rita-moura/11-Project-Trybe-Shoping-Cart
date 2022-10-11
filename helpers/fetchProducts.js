e6f3f0e0e697336bf8f9dc18419531bb0ae10cf6
Rita Moura <ritamoura520@gmail.com>
2022-10-11 17:35:48 -0300

    Requisito 1

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,6 +1,10 @@
-const fetchProducts = () => {
+const fetchProducts = async () => {
   // seu código aqui
+  const response = await fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador');
+  const data = await response.json();
+  return data;
 };
+console.log(fetchProducts());
 
 if (typeof module !== 'undefined') {
   module.exports = {
--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -3,6 +3,16 @@ const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fetchProducts', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  test('Verifica se fetchProducts é uma função', async () => {
+    expect(typeof fetchProducts).toBe('function');
+  });
+  test('Verifica se a fetch foi chamada', async () => {
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalled();
+  })
+  test('Verifica se a fetch foi chamada com endpoint correto', async () => {
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/sites/MLB/search?q=computador');
+  })
+  // fail('Teste vazio');
 });
